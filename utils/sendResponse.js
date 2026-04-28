772f825eaffda4264f9023cb6c65a670e209d0da
Abhay Kumar <abhay0480sharma@gmail.com>
2026-04-28 14:12:12 +0530

    add Access-Control-Allow-Origin

--- utils/sendResponse.js
+++ utils/sendResponse.js
@@ -3,5 +3,7 @@
 export const sendResponse = (res, statusCode, contentType, payload) => {
     res.statusCode = statusCode
     res.setHeader('Content-Type', contentType)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET')
     res.end(payload)
 }
